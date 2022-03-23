23a14eeef54e5ef32dd156684b47ae29174af59e
Semyon Zasov <semenmaslina@gmail.com>
2022-03-23 23:06:30 +0300

    сделал верстку для страницы Main

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,14 @@ import './App.css';
 import { Navbar } from './components/navbar/Navbar';
 import { Menu} from "./components/menu/Menu"
 import { MenuActive } from "./components/menu/MenuActive"
+import { Main } from "./pages/main/Main"
 
 function App() {
   return (
     <>
-      
       <Navbar/>
       <Menu></Menu>
+      <Main></Main>
       {/* <MenuActive></MenuActive> */}
       </>
   );
