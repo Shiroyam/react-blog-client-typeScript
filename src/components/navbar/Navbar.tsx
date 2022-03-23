616df7e62d455aab9f16512cf76734758ef1275d
Semyon Zasov <semenmaslina@gmail.com>
2022-03-23 20:56:49 +0300

    сделал верстку для Navbar

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,18 @@
+import React from "react"
+import "./navbar.scss"
+import SearchIcon from '@mui/icons-material/Search';
+import PersonIcon from '@mui/icons-material/Person';
+import CreateIcon from '@mui/icons-material/Create';
+
+export const Navbar: React.FC = () =>{
+    return(
+    <nav className="nav">
+        <div className="nav__header">VASYA BLOG</div>
+        <div className="nav__icon">
+            <SearchIcon className="nav__iconSearch"/>
+            <PersonIcon className="nav__iconPerson"/>
+            <CreateIcon className="nav__iconCreate"/>
+        </div>
+    </nav>
+    )
+}
